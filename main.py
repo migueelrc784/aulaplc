f9fb73eebb2ceb4a7260846d4a34fa85863c558f
Miguel Ramirez <angelchandia0098@gmail.com>
2026-05-20 19:27:26 -0400

    separacion css y js

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
 from fastapi import FastAPI, Request
-from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+from pathlib import Path
 
 app = FastAPI()
 
-app.mount("/static", StaticFiles(directory="static"), name="static")
+BASE_DIR = Path(__file__).resolve().parent
 
-templates = Jinja2Templates(directory="templates")
+templates = Jinja2Templates(
+    directory=str(BASE_DIR / "templates")
+)
+
+app.mount(
+    "/static",
+    StaticFiles(directory="static"),
+    name="static"
+)
 
 @app.get("/")
 def inicio(request: Request):
 
     return templates.TemplateResponse(
-        "index.html",
-        {"request": request}
+        request,
+        "index.html"
     )
