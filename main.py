092e7b1cb2979090978ae37a010182b636ca7d67
migueelrc784 <angelchandia0098@gmail.com>
2026-05-20 20:50:05 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,13 +3,18 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from pathlib import Path
 
-app = FastAPI()
-
 BASE_DIR = Path(__file__).resolve().parent
 
-app.mount("/static", StaticFiles(directory=str(BASE_DIR / "static")), name="static")
+app = FastAPI()
+
+app.mount(
+    "/static",
+    StaticFiles(directory=str(BASE_DIR / "static")),
+    name="static"
+)
 
 templates = Jinja2Templates(directory=str(BASE_DIR / "templates"))
+
 
 @app.get("/")
 async def home(request: Request):
@@ -17,3 +22,6 @@ async def home(request: Request):
         "index.html",
         {"request": request}
     )
+
+# FORZAR EXPORT (IMPORTANTE PARA VERCEL)
+app = app
