b6d01dded276957b83a22f33788174be3df00cb6
migueelrc784 <angelchandia0098@gmail.com>
2026-05-20 21:40:03 -0400

    Update tank.js

--- static/js/tank.js
+++ static/js/tank.js
@@ -1,10 +1,39 @@
-let fillLevel=0
-let valveOpen=false
+let tankLevel=40
 
-openValve()
+function fillTank(){
 
-closeValve()
+if(tankLevel<100){
 
-setInterval(()=>{
+tankLevel+=10
 
-},50)
+updateTank()
+
+log("Tanque llenando")
+
+}
+
+}
+
+function emptyTank(){
+
+if(tankLevel>0){
+
+tankLevel-=10
+
+updateTank()
+
+log("Tanque vaciando")
+
+}
+
+}
+
+function updateTank(){
+
+document.getElementById("water")
+.style.height=tankLevel+"%"
+
+document.getElementById("tankValue")
+.innerHTML=tankLevel+"%"
+
+}
