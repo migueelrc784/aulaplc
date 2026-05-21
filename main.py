b04f9447995c12d19bb7fdb41e8c038c5eb45509
migueelrc784 <angelchandia0098@gmail.com>
2026-05-20 21:06:00 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from pathlib import Path
 
@@ -6,14 +7,19 @@ app = FastAPI()
 
 BASE_DIR = Path(__file__).resolve().parent
 
+app.mount(
+    "/static",
+    StaticFiles(directory=str(BASE_DIR / "static")),
+    name="static"
+)
+
 templates = Jinja2Templates(
     directory=str(BASE_DIR / "templates")
 )
 
 @app.get("/")
-def inicio(request: Request):
-
+async def home(request: Request):
     return templates.TemplateResponse(
-        request,
-        "index.html"
+        "index.html",
+        {"request": request}
     )
