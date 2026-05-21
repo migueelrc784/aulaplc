9392f508755392c324e86b2c421334ed49fcd512
Miguel Ramirez <angelchandia0098@gmail.com>
2026-05-20 20:24:48 -0400

    descripcion

--- main.py
+++ main.py
@@ -7,12 +7,9 @@ app = FastAPI()
 
 BASE_DIR = Path(__file__).resolve().parent
 
-# Static files (CSS, JS)
 app.mount("/static", StaticFiles(directory=str(BASE_DIR / "static")), name="static")
 
-# Templates
 templates = Jinja2Templates(directory=str(BASE_DIR / "templates"))
-
 
 @app.get("/")
 def inicio(request: Request):
